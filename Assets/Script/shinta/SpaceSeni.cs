9a42a551e50614214df9c4efc0ea6324cf03030c
MashiroSuzuki <77772201+suzukimashiro@users.noreply.github.com>
2021-12-03 23:05:46 +0900

    no message

---
+++ Assets/Script/shinta/SpaceSeni.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceSeni : MonoBehaviour
+{
+    //移動するシーンの名前
+    public string nextSceneName = "";
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //spaceが押されたとき
+        if (Input.GetKeyDown("space"))
+        {
+            //指定したシーンへ遷移する
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
